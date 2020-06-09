197af0796df8e207c583c5c121aaea2b52b3d1ce
BalduinLandolt <balduin.landolt@hotmail.com>
2020-06-09 20:42:33 +0200

    Feature: Add static collection response

--- dts_mimicer/pyDTS/collections.py
+++ dts_mimicer/pyDTS/collections.py
@@ -1,6 +1,3 @@
-from flask import jsonify
-
-
 class Collections:
 
     def __init__(self, path, prefix=""):
@@ -12,8 +9,41 @@ class Collections:
         return self.__path
 
     @property
-    def collection_reply(self):
-        return ["todo!"]
+    def collection_reply(self):  # TODO: make dynamic! (will need classes `Collection` and `Resource`)
+        return {
+            "@context": {
+                "@vocab": "https://www.w3.org/ns/hydra/core#",
+                "dc": "http://purl.org/dc/terms/",
+                "dts": "https://w3id.org/dts/api#"
+            },
+            "@id": "sample_collection",
+            "totalItems": 2,
+            "dts:totalParents": 0,
+            "dts:totalChildren": 2,
+            "@type": "Collection",
+            "title": "A sample collection made up for this mimicker",
+            "description": "Contains two sample children",
+            "mapping": [
+                {
+                    "@id": "sample_01",
+                    "title": "Sample 01",
+                    "description": "First sample Collection",
+                    "@type": "Collection",
+                    "totalItems": 1,
+                    "dts:totalParents": 1,
+                    "dts:totalChildren": 1
+                },
+                {
+                    "@id": "sample_02",
+                    "title": "Sample 02",
+                    "description": "Second sample Collection",
+                    "@type": "Collection",
+                    "totalItems": 1,
+                    "dts:totalParents": 1,
+                    "dts:totalChildren": 1
+                }
+            ]
+        }
 
     @property
     def host_prefix(self):
@@ -24,7 +54,7 @@ class Collections:
         self.__host_prefix = prefix
 
     @property
-    def absolut_path(self):
+    def absolute_path(self):
         p = self.host_prefix + self.collection_path
-        #p = p.replace("//", "/")
+        # p = p.replace("//", "/")
         return p
